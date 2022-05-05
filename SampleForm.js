ef9f3d9c659105db63ec3f2ca0dfdcb538ef7165
Jagadeesh23520 <82135265+Jagadeesh23520@users.noreply.github.com>
2022-05-05 14:13:35 +0530

    Form Handling Concept

--- SampleForm.js
+++ SampleForm.js
@@ -11,7 +11,7 @@ class SampleForm extends Component{
             country :''
         }
     }
-    
+   
     handleFirstName=(event)=>{
         this.setState({
             firstName:event.target.value
